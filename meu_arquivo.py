da1d17aa0447dd2ad9a102965eeebb3dad34587c
Andre R M Conforti <andre.r.m.conforti@gmail.com>
2022-01-03 11:58:05 -0300

    Ticket médio
    calculando o valor do ticket médio

--- meu_arquivo.py
+++ meu_arquivo.py
@@ -12,12 +12,16 @@ pd.set_option('display.max_columns', None)
 # Faturamento por loja------------------------------------------------------------------------------------------------
 faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
 print(faturamento)
+print('-' * 50)
 
 # Quantidade de produtos vendidos por loja----------------------------------------------------------------------------
 quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
 print(quantidade)
+print('-' * 50)
 
 # Ticket Médio por produto em cada loja (Faturamento / Quantidade de produtos vendidos)-------------------------------
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()  #.to_frame transforma os dados em uma tabela
+print(ticket_medio)
 
 # Enviar um email com o relatório-------------------------------------------------------------------------------------
 
